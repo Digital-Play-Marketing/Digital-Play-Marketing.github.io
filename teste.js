e492e38caf76309cbf7d7f418fd5ea7e1b9dc24c
Kauan Novello <50461490+KauanCoop@users.noreply.github.com>
2020-05-04 20:03:34 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -2,13 +2,13 @@ function inicio(){
     // alert('você tocou em inicio');
     var display = document.getElementsByClassName('display');
     $(display).empty();
-    $(display).append("<iframe width='100%' height='100%' src='https://www.youtube.com/embed/tNttghXo9ZM' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>")
+    $(display).append("<iframe width='100%' height='100%' src='https://www.youtube.com/embed/EGByz3yDyAI' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>")
 }
 function serviços(){
     // alert('você tocou em serviços');
     var display = document.getElementsByClassName('display');
     $(display).empty();
-    $(display).append(" <iframe width='100%' height='100%' src='https://www.youtube.com/embed/3kPplHPryFk' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>");
+    $(display).append(" <iframe width='100%' height='100%' src='https://www.youtube.com/embed/kvbHlmdYFvg' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>");
          
 }
 function sobreMim(){
@@ -16,7 +16,7 @@ function sobreMim(){
     var display = document.getElementsByClassName('display');
     $(display).empty();
     $(display).append();
-    $(display).append(" <iframe width='100%' height='100%' src='https://www.youtube.com/embed/6SAu5H21LaI' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>");
+    $(display).append(" <iframe width='100%' height='100%' src='https://www.youtube.com/embed/BG2FN0Z9rEc' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>");
 }
 function contato(){
     // alert('você tocou em contato');

2f94c9ee6dbfd35dad4872933f84cdabad6903f0
Kauan Novello <50461490+KauanCoop@users.noreply.github.com>
2020-05-06 06:23:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function scrollFunction() {
   if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
     document.getElementById("header").style.transition = "0.5s";
     document.getElementById("header").style.height = "75px";
-    document.getElementById("logo").src = "logo2.jpg";
+    document.getElementById("logo").src = "logo2.JPG";
     document.getElementById("logo").style.height = "70px";
     document.getElementById("logo").style.width = "75px";
   } else {
